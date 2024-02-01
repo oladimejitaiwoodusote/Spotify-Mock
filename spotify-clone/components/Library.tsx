745e4cf15b42c6a077a24507a6b0e35f8b5dbe9b
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2024-02-01 06:50:02 -0600

    Sidebar v2

--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { TbPlaylist} from 'react-icons/tb'
+import { AiOutlinePlus} from 'react-icons/ai'
 
 const Library = () => {
     const onClick = () => {
@@ -20,17 +21,42 @@ const Library = () => {
             >
               <div
                 className="
-                  infline-flex
+                  inline-flex
                   items-center
                   gap-x-2
                 "
               >
                 <TbPlaylist className="text-neutral-400" size={26}/>
-                <p>
+                <p
+                  className="
+                    text-neutral-400
+                    font-medium
+                    text-md
+                  "
+                >
                     Your Library
                 </p>
               </div>
-                
+            <AiOutlinePlus 
+               onClick={onClick} 
+               size={20}
+               className="
+                 text-neutral-400
+                 cursor-pointer
+                 hover:text-white
+                 transition
+               "
+
+            />
+            </div>
+            <div className="
+                flex
+                flex-col
+                gap-y-2
+                mt-4
+                px-3
+            ">
+                List of Songs!
             </div>
         </div>
     )
