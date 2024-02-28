b23b2a2cfa993ce5401f7ecea46a2936591b68ea
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2024-02-28 09:51:09 -0600

    Songs Fetching v0

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -1,3 +1,4 @@
+import getSongsByUserId from "@/actions/getSongsByUserId";
 import Sidebar from "@/components/Sidebar";
 import ModalProvider from "@/providers/ModalProvider";
 import SupabaseProvider from "@/providers/SupabaseProvider";
@@ -14,11 +15,14 @@ export const metadata: Metadata = {
   description: "Listen to music!",
 };
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -26,7 +30,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider/>
-            <Sidebar>
+            <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
           </UserProvider>
