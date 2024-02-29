8b92b9bf416a6662d77fb8fac07ef86016397bb8
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2024-02-29 09:46:01 -0600

    Songs Fetching v1

--- spotify-clone/components/MediaItem.tsx
+++ spotify-clone/components/MediaItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import Image from "next/image";
 
 interface MediaItemProps {
     data: Song;
@@ -26,9 +27,46 @@ const MediaItem: React.FC<MediaItemProps> = ({
     <div
         onClick={handleClick}
         className="
+          flex
+          items-center
+          gap-x-3
+          cursor-pointer
+          hover:bg-neutral-800/50
+          w-full
+          p-2
+          rounded-md
+
         "
     >
-        
+        <div
+          className="
+            relative
+            rounded-md
+            min-h-[48px]
+            min-w-[48px]
+            overflow-hidden
+          "
+        >
+          <Image
+            fill
+            src={imageUrl || '/images/liked.png'}
+            alt="Media Item"
+            className="object-cover"
+          />
+        </div>
+        <div className="
+          flex
+          flex-col
+          gap-y-1
+          overflow-hidden
+        ">
+          <p className="text-white truncate">
+            {data.title}
+          </p>
+          <p className="text-neutral-400 text-sm truncate">
+            {data.author}
+          </p>
+        </div>
     </div>
   )
 }
