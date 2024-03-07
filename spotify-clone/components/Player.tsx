acf814617f7e9b6c7377ce056e7decfaa0971bc9
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2024-03-07 09:55:49 -0600

    Player functionality v0

---
+++ spotify-clone/components/Player.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import useGetSongById from "@/hooks/useGetSongById";
+import usePlayer from "@/hooks/usePlayer";
+
+const Player = () => {
+    const player = usePlayer();
+    const {song} = useGetSongById(player.activeId);
+
+
+    return (
+        <div>
+            Player!
+        </div>
+    );
+}
+
+export default Player;
